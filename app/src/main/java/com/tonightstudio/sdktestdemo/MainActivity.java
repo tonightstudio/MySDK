ab61c70d6db63b6a0f614c8cb1e36b0bcdde9531
李艺为 <15897461476@139.com>
2017-11-09 12:34:35 +0800

    1.0

--- app/src/main/java/com/tonightstudio/sdktestdemo/MainActivity.java
+++ app/src/main/java/com/tonightstudio/sdktestdemo/MainActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.tonightstudio.mysdk.ToastUtils;
+import group.tonight.ToastUtils;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
